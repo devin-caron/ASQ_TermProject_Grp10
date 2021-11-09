6b05f6f609c87c49ceec2de03fcd61e542c75519
SemiDoge <taelforlife@gmail.com>
2021-11-09 14:42:57 -0500

    Created a class called FlightData this object will contain all of the telemetry entries for a flight.

---
+++ AircraftTransmissionSystem/FlightData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AircraftTransmissionSystem
+{
+    class FlightData
+    {
+        private static List<AircraftTelemetryEntry> telemetryList;
+        private static String flightName = "";
+        public FlightData(String name) {
+            flightName = name;
+        }
+
+        public AircraftTelemetryEntry getEntry(int index) {
+            if (index > telemetryList.Count) {
+                AircraftTelemetryEntry ret = null;
+                return ret;
+            }
+
+            return telemetryList[index];
+        }
+
+        public void addEntry(AircraftTelemetryEntry entry) {
+            telemetryList.Add(entry);
+        }
+
+
+        public static List<AircraftTelemetryEntry> TelemetryList => telemetryList;
+        public static string FlightName => flightName;
+    }
+}
