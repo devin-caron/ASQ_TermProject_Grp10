fa587156c026e01d426bd2101563389dcde3d4c7
SemiDoge <taelforlife@gmail.com>
2021-11-09 20:17:05 -0500

    Implemented main, readTelemetry, and readFile functions.

--- AircraftTransmissionSystem/ATTS.cs
+++ AircraftTransmissionSystem/ATTS.cs
@@ -1,23 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AircraftTransmissionSystem
 {
     class ATTS {
-        private List<AircraftTelemetryEntry> telemetryList;
-        
-        static void Main(string[] args)
-        {
+        private static List<FlightData> flights = new List<FlightData>();
+
+        static void Main(string[] args) {
+            readTelemetry();
         }
 
-        static AircraftTelemetryEntry readTelemetry() {
+        static void readTelemetry() {
+            String[] filePaths;
+            String flightDir = Path.GetFullPath(Path.Combine(@"..\..\..\AircraftTransmissionData\"));
 
-            return 0;
+            filePaths = Directory.GetFiles(flightDir); 
+            int numOfFiles = filePaths.Length;
+
+            //Console.WriteLine();
+
+            for (int i = 0; i < numOfFiles; i++) {
+                flights.Add(readFile(filePaths[i]));
+                Console.WriteLine(flights[i].FlightName);            
+            }
+
+            Console.ReadLine();
         }
 
+    static FlightData readFile(String path) {
+            String flightFilename = path.Substring(path.LastIndexOf('\\'));
+            FlightData fd = new FlightData(flightFilename);
+            
+            try {
+                using (StreamReader reader = new StreamReader(path)) {
+                    String ln;
+
+                    while ((ln = reader.ReadLine()) != null) {
+                        if (ln == "") {
+                            break;
+                        }
+
+                        AircraftTelemetryEntry newEntry = new AircraftTelemetryEntry(ln);
+                        fd.addEntry(newEntry);
+                    }
+                }
+            }
+            catch(Exception e) {
+                Console.WriteLine("ATTS::readFile() encountered: " + e.Message);
+            }
+
+            return fd;
+        }
+    
+       
     }
-
 }
