e11cfd3c193272eb8bbb22c6e9c7cd1bf983d198
DusanS <dusan.sasic7@gmail.com>
2021-11-11 15:30:05 -0500

    Redo of the insert functions

--- ASQ_TermProject_Grp10/DbManager.cs
+++ ASQ_TermProject_Grp10/DbManager.cs
@@ -23,6 +23,63 @@ namespace ASQ_TermProject_Grp10
         }
 
 
+        public void InserAttitudeParameters(string aircraftID, string timestamp, double altitude, double pitch, double bank)
+        {
+            SqlCommand cmd = null;
+
+            try
+            {
+                conn.Open();
+
+                cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO[dbo].[AttitudeParameters]([AircraftID],[Timestamp],[Altitude],[Pitch],[Bank]) " +
+                    "VALUES('"+aircraftID+"','"+timestamp+"',"+altitude+","+pitch+","+bank+")";
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+        }
+
+
+
+
+        public void InsertGForceParameters(string aircraftID, string timestamp, double x, double y, double z, double weight)
+        {
+            SqlCommand cmd = null;
+
+
+            
+            try
+            {
+                conn.Open();
+
+                cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO[dbo].[GForceParameters]([AircraftID],[Timestamp],[AccelX],[AccelY],[AccelZ],[Weight]) " +
+                    "VALUES('" + aircraftID + "','" + timestamp + "'," + x + "," + y + "," + z + ", "+weight+")";
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+        }
+
         public DataTable ReadFromDb(string tblName)
         {
 
