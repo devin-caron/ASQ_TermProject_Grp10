90a07694a7c86cc358a03fb41d9eb4b8801a11e5
devcar <devmcaron@gmail.com>
2021-12-09 14:53:33 -0500

    Added Database unit testing files

---
+++ Database.Tests/DbUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using ASQ_TermProject_Grp10;
+
+namespace Database.Tests
+{
+    [TestClass]
+    public class DbUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
